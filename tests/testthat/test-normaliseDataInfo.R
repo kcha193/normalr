088f13e04f7d3e1ff3f0f4e801ca4160e8305530
Kevin Chang <kevin.ct.chang@gmail.com>
2020-11-22 00:43:00 +1300

    more tests

--- tests/testthat/test-normaliseDataInfo.R
+++ tests/testthat/test-normaliseDataInfo.R
@@ -24,6 +24,29 @@ test_that("multiplication works", {
                         function(x) normalise(x[-1], x[1])[-c(1:2)])))
 
 
+  res <-  normaliseDataInfo(mtcars, getLambda(mtcars, parallel = TRUE))
+
+
+  expect_equal(length(res), 3)
+
+  lambdas <-
+    getLambda(mtcars, parallel = FALSE)
+
+  names(lambdas) <- names(mtcars)
+
+  expect_equal(res$lambdas, lambdas)
+
+  expect_equal(round(sum(res$scaled)), 638)
+
+
+  expect_equal(round(sum(res$transDat)), 15327)
+
+  expect_equal(
+    res$transDat,
+    as.data.frame(apply(rbind(lambdas, mtcars), 2,
+                        function(x) normalise(x[-1], x[1])[-c(1:2)])))
+
+
 
 
 })
